4f70c3bfe860757b3575033447fab006f7cd66ac
Paul Endri <abrecan.eyndrill@gmail.com>
2021-01-11 23:51:11 -0500

    reenable show player

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -12,61 +12,64 @@ const LayoutComponent = ({ children }: any) => {
     history.push(`/players/${search}`);
   };
 
-  useCallback(() => {
-    const el = document.getElementById("SHOW_PLAYER_SEARCH");
+  const el = useCallback(() => {
+    const el = window.document.getElementById("SHOW_PLAYER_SEARCH");
 
+    console.log(el);
     if (el) {
       updatePlayValue(el.getAttribute("data-enabled") === "true");
     }
   }, []);
 
   return (
-    <Container fluid style={{ marginLeft: "0px !important", marginRight: "0px !important" }}>
-      <Segment inverted style={{ marginBottom: 0, borderRadius: 0, padding: 0 }} raised={true}>
-        <Menu inverted>
-          <Menu.Item header as={Link} to="/">
-            <Image src={getImageSrc("icon")} size="mini" />
-            Surival Guide
-          </Menu.Item>
+    <div ref={el}>
+      <Container fluid style={{ marginLeft: "0px !important", marginRight: "0px !important" }}>
+        <Segment inverted style={{ marginBottom: 0, borderRadius: 0, padding: 0 }} raised={true}>
+          <Menu inverted>
+            <Menu.Item header as={Link} to="/">
+              <Image src={getImageSrc("icon")} size="mini" />
+              Surival Guide
+            </Menu.Item>
 
-          <Menu.Item as={Link} to="/wiki/*">
-            Wiki
-          </Menu.Item>
-          <Menu.Item as={Link} to="/planner">
-            Planner
-          </Menu.Item>
-          <Menu.Item as={Link} to="/about">
-            About
-          </Menu.Item>
-          {showPlayValue && (
-            <Input
-              action={{
-                color: "orange",
-                labelPosition: "left",
-                icon: "search",
-                content: "Search",
-                onClick: handleSearchClick,
-              }}
-              size="small"
-              style={{ margin: "auto", paddingRight: "25%" }}
-              onChange={(e) => {
-                updateSearch(e.target.value);
-              }}
-              placeholder="search for player"
-            />
-          )}
-        </Menu>
-      </Segment>
+            <Menu.Item as={Link} to="/wiki/*">
+              Wiki
+            </Menu.Item>
+            <Menu.Item as={Link} to="/planner">
+              Planner
+            </Menu.Item>
+            <Menu.Item as={Link} to="/about">
+              About
+            </Menu.Item>
+            {showPlayValue && (
+              <Input
+                action={{
+                  color: "orange",
+                  labelPosition: "left",
+                  icon: "search",
+                  content: "Search",
+                  onClick: handleSearchClick,
+                }}
+                size="small"
+                style={{ margin: "auto", paddingRight: "25%" }}
+                onChange={(e) => {
+                  updateSearch(e.target.value);
+                }}
+                placeholder="search for player"
+              />
+            )}
+          </Menu>
+        </Segment>
 
-      <Segment basic style={{ paddingLeft: 0, paddingTop: 0, paddingRight: 0, margin: 0 }}>
-        {children}
-      </Segment>
-      <Segment color="black" inverted style={{ border: 0 }} basic attached="bottom">
-        Lumia Survival Guide and co. are in no way affiliated with Nimble Neuron, Eternal Return:
-        Black Survival, or any related entity. For questions and support email the
-        <a href="mailto:jrs.abrecan@gmail.com"> the site administrator.</a>
-      </Segment>
-    </Container>
+        <Segment basic style={{ paddingLeft: 0, paddingTop: 0, paddingRight: 0, margin: 0 }}>
+          {children}
+        </Segment>
+        <Segment color="black" inverted style={{ border: 0 }} basic attached="bottom">
+          Lumia Survival Guide and co. are in no way affiliated with Nimble Neuron, Eternal Return:
+          Black Survival, or any related entity. For questions and support email the
+          <a href="mailto:jrs.abrecan@gmail.com"> the site administrator.</a>
+        </Segment>
+      </Container>
+    </div>
   );
 };
 
